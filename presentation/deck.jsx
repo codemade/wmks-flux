d14559b3ada3738bb060686b320ca1fd1ce9dd9a
René Viering <rene.viering@gmail.com>
2015-10-31 15:23:46 +0100

    Add some slides.

--- presentation/deck.jsx
+++ presentation/deck.jsx
@@ -17,7 +17,12 @@ const images = {
   chrkhl: require("./chrkhl.png"),
   revrng: require("./revrng.png"),
   twitter: require("./twitter.svg"),
-  github: require("./github.svg")
+  github: require("./github.svg"),
+  caffeine: require("./img/caffeine.png"),
+  caffeine_components: require("./img/caffeine_components.png"),
+  view_action_store: require("./img/view_action_store.png"),
+  view_action_store_dispatcher: require("./img/view_action_store_dispatcher.png"),
+  view_action_store_event: require("./img/view_action_store_event.png")
 };
 
 const styles = {
@@ -28,6 +33,15 @@ const styles = {
 };
 
 preloader([images.city, images.kat]);
+
+const buttonStyles = {
+  backgroundColor: '#3498db',
+  border: 'none',
+  borderRadius: '4px',
+  fontSize: '20px',
+  padding: '10px',
+  color: 'white'
+};
 
 export default class extends React.Component {
   render() {
@@ -90,6 +104,263 @@ export default class extends React.Component {
             </BlockQuote>
           </Appear>
         </Slide>
+
+        <Slide bgColor="#e74c3c">
+          <Heading size={2} textColor="white">
+            # no framework
+          </Heading>
+        </Slide>
+
+        <Slide bgColor="#e74c3c">
+          <Heading size={2} textColor="white">
+            # no library
+          </Heading>
+        </Slide>
+
+        <Slide bgColor="#27ae60">
+          <Heading size={2} textColor="white">
+            # just a pattern
+          </Heading>
+        </Slide>
+
+        <Slide transition={["slide"]}>
+          <Heading size={1} fit textColor="quartary" textFont="secondary">
+            Why Flux ?
+          </Heading>
+        </Slide>
+
+        <Slide transition={["slide"]}>
+          <BlockQuote>
+            <Quote>
+              No one knows how to structure a front-end application
+            </Quote>
+          </BlockQuote>
+        </Slide>
+
+        <Slide transition={["slide"]}>
+          <Heading size={1} fit>
+            libraries/frameworks are taught
+          </Heading>
+          <Heading size={2} fit>
+            never BEST PRACTICES
+          </Heading>
+        </Slide>
+
+        <Slide>
+          <Heading size={1} fit>
+            The real problem:
+          </Heading>
+          <Appear>
+            <Heading size={2} fit textColor="secondary">
+              Data Flow
+            </Heading>
+          </Appear>
+        </Slide>
+
+        <Slide bgImage={images.caffeine} bgDarken="0.9">
+          <Link style={buttonStyles} href="http://codemade.js.org/caffeine">Go to coffee store</Link>
+        </Slide>
+
+        <Slide bgImage={images.caffeine_components}></Slide>
+
+        <Slide bgImage={images.caffeine} bgDarken="0.9">
+          <Heading size={1}>
+            use cases
+          </Heading>
+          <List>
+            <ListItem><Appear fid="1">Show all products</Appear></ListItem>
+            <ListItem><Appear fid="2">Add article to shopping cart (and actualize shopping cart)</Appear></ListItem>
+            <ListItem><Appear fid="3">Filter articles by intensity</Appear></ListItem>
+            <ListItem><Appear fid="4">Show all articles in the shopping cart</Appear></ListItem>
+            <ListItem><Appear fid="5">Increase/decrease article amount</Appear></ListItem>
+            <ListItem><Appear fid="6">Redeem a coupon</Appear></ListItem>
+          </List>
+        </Slide>
+
+        <Slide bgImage={images.caffeine} bgDarken="0.95">
+          <Heading size={2} fit>
+            Add article to shopping cart
+          </Heading>
+          <Appear>
+            <Heading size={2} textColor="white">
+              How does it work?
+            </Heading>
+          </Appear>
+        </Slide>
+
+        <Slide bgImage={images.caffeine} bgDarken="0.95">
+          <Heading size={2} fit>
+            The user sees the article list
+          </Heading>
+          <Appear>
+            <Heading size={2} textColor="white">
+              # view
+            </Heading>
+          </Appear>
+        </Slide>
+
+        <Slide bgImage={images.caffeine} bgDarken="0.95">
+          <Heading size={2} fit>
+            The users clicks on an article to add
+          </Heading>
+          <Appear>
+            <Heading size={2} textColor="white">
+              # action
+            </Heading>
+          </Appear>
+        </Slide>
+
+        <Slide bgImage={images.caffeine} bgDarken="0.95">
+          <Heading size={2} fit>
+            It needs to be saved, that the article has been added.
+          </Heading>
+          <Appear>
+            <Heading size={2} textColor="white">
+              # store
+            </Heading>
+          </Appear>
+        </Slide>
+
+
+
+        <Slide bgImage={images.caffeine} bgDarken="0.95">
+          <Heading size={2} fit>
+            Redeem a coupon
+          </Heading>
+          <Appear>
+            <Heading size={2} textColor="white">
+              How does it work?
+            </Heading>
+          </Appear>
+        </Slide>
+
+        <Slide bgImage={images.caffeine} bgDarken="0.95">
+          <Heading size={2} fit>
+            The user sees the shopping cart
+          </Heading>
+          <Appear>
+            <Heading size={2} textColor="white">
+              # view
+            </Heading>
+          </Appear>
+        </Slide>
+
+        <Slide bgImage={images.caffeine} bgDarken="0.95">
+          <Heading size={2} fit>
+            The users adds the coupon code and clicks 'redeem'
+          </Heading>
+          <Appear>
+            <Heading size={2} textColor="white">
+              # action
+            </Heading>
+          </Appear>
+        </Slide>
+
+        <Slide bgImage={images.caffeine} bgDarken="0.95">
+          <Heading size={2} fit>
+            The total price must be reduced by 15%
+          </Heading>
+          <Appear>
+            <Heading size={2} textColor="white">
+              # store
+            </Heading>
+          </Appear>
+        </Slide>
+
+        <Slide bgImage={images.caffeine} bgDarken="0.95">
+          <Image src={images.view_action_store} width="900" />
+          <Appear>
+            <Heading size={1} textColor="white" fit>
+              FLUX
+            </Heading>
+          </Appear>
+        </Slide>
+
+        <Slide bgImage={images.caffeine} bgDarken="0.95">
+          <Image src={images.view_action_store} width="900" />
+          <br/><br/>
+            <Heading size={1} textColor="white" fit>
+              How did the infos of actions get into the store?
+            </Heading>
+        </Slide>
+
+        <Slide bgImage={images.caffeine} bgDarken="0.95">
+          <Image src={images.view_action_store} width="900" />
+          <br/><br/>
+            <Heading size={1} textColor="white" fit>
+              How many stores are there?
+            </Heading>
+        </Slide>
+
+        <Slide bgImage={images.caffeine} bgDarken="0.95">
+          <Image src={images.view_action_store} width="900" />
+          <br/><br/>
+            <Heading size={1} textColor="white" fit>
+              What if an action is relevant for more than one store?
+            </Heading>
+        </Slide>
+
+
+        <Slide bgImage={images.caffeine} bgDarken="0.99">
+          <Image src={images.view_action_store_dispatcher} width="900"/>
+        </Slide>
+
+
+        <Slide>
+          <Image src={images.view_action_store_dispatcher} width="900"/>
+          <Heading size={1} fit>
+            How did a view notice, that anything changed?
+          </Heading>
+        </Slide>
+
+        <Slide>
+          <Image src={images.view_action_store_dispatcher} width="900"/>
+          <Heading size={1} fit>
+            i.e. the shopping card re-renders, if an article has been added!?
+          </Heading>
+        </Slide>
+
+        <Slide>
+          <Heading size={1} fit>
+            Do it like a mailing list.
+          </Heading>
+        </Slide>        
+
+        <Slide bgImage={images.caffeine} bgDarken="0.95">
+          <Image src={images.view_action_store_event} width="800"/>
+          <Appear>
+            <Heading size={2} fit>
+              If there is something new, send a mail to all subscribers.
+            </Heading>
+          </Appear>
+          <Appear>
+            <Heading size={2} textColor="white">
+              # Store fires an event
+            </Heading>
+          </Appear>
+        </Slide> 
+
+        <Slide bgImage={images.caffeine} bgDarken="0.95">
+          <Image src={images.view_action_store_event} width="800"/>
+          <Heading size={2} fit>
+            Subscribers get notified and pull the new mails. 
+          </Heading>
+          <Appear>
+            <Heading size={2} textColor="white">
+              # the view gets notified
+            </Heading>
+          </Appear>
+        </Slide> 
+
+        <Slide bgImage={images.caffeine} bgDarken="0.95">
+          <Image src={images.view_action_store_event} width="800"/>
+          <Heading size={2} fit>
+            Subscribers get notified and pull the new mails. 
+          </Heading>
+            <Heading size={2} textColor="white" fit>
+              # and pulls new data from the store.
+            </Heading>
+        </Slide> 
       </Deck>
     );
   }
